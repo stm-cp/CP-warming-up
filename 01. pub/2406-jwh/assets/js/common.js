adbd7510420d8177e13eb3537909db51b6e295ed
jwh-codepar <140412872+thumb-yeongo@users.noreply.github.com>
2024-06-27 17:43:30 +0900

    jwh: 0627

--- 01. pub/2406-jwh/assets/js/common.js
+++ 01. pub/2406-jwh/assets/js/common.js
@@ -1,8 +1,19 @@
-//워밍업
-function 워밍업() {
-
-}
-
-$(document).ready(function () {
-  워밍업() // 간단한 역할을 설명
+window.addEventListener('load', () => {
+  ReviewSet(0); // 첫번째 리뷰 보여주기.
 });
+
+// Recent Reviews 섹션에서 이미지 버튼을 눌렀을 때 해당하는 리뷰를 세팅.
+function ReviewSet(idx){
+  const listLength = document.querySelectorAll('.img_btn_wrap ul li').length;
+
+  // 인덱스와 동일하면 li의 class에 active add, 아니라면 active remove.
+  for(let i = 0; i < listLength; i++) {
+    if(i === idx) {
+      document.querySelectorAll('.img_btn_wrap ul li')[i].classList.add('active');
+      document.querySelectorAll('.reviews_txt_wrap ul li')[i].classList.add('active');
+    } else {
+      document.querySelectorAll('.img_btn_wrap ul li')[i].classList.remove('active');
+      document.querySelectorAll('.reviews_txt_wrap ul li')[i].classList.remove('active');
+    }
+  }
+}
