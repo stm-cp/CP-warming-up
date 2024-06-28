3fc74eb2be6e6be75b6a8c9f3cc1eba9534fb100
yoonssstar <ssstar@codepartners.co.kr>
2024-06-28 17:57:42 +0900

    ys: 0628

--- 01. pub/2406_ssstar/assets/js/common.js
+++ 01. pub/2406_ssstar/assets/js/common.js
@@ -1,15 +1,31 @@
 
 
 $(document).ready(function () {
+  // header sticky
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 60) {
+      $('header').addClass('sticky');
+    } else {
+      $('header').removeClass('sticky');
+    }
+  });
+
+  // header gnb
+  $('.gnb_list > li').hover(
+    function () {
+      $(this).find('.gnb_sub_list').addClass('active');
+    },
+    function () {
+      $(this).find('.gnb_sub_list').removeClass('active');
+    }
+  );
   // tab action
-  $('.tab_list_item').on('click',function(e){
+  $('.tab_list_item').on('click', function (e) {
     e.preventDefault();
-    
     var Idx = $(this).index();
     $('.tab_list_item').removeClass('active');
     $(this).addClass('active');
     $('.tabview_list_item').removeClass('active').eq(Idx).addClass('active')
-    
   })
 });
 
